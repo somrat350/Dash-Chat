f710091796982bc5f61283caf2e7d1bda7aa9825
sabbirsohag-1509 <sabbirhossainsohag1509@gmail.com>
2026-02-15 14:54:09 +0600

    Navbar Design Completed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import "./index.css";
+import router from "../routes/Router";
+import { RouterProvider } from "react-router";
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
