5f40766d2befd3dbb8bd11a91b436b598e548173
china akther <chinaakther05@gmail.com>
2026-02-17 21:15:10 +0600

    Complete the About page with 4 features.

---
+++ frontend/src/pages/public/About.jsx
@@ -0,0 +1,15 @@
+import AboutHero from "../../components/public/about/AboutHero";
+import Location from "../../components/public/about/Location";
+
+
+
+const About = () => {
+    return (
+        <div>
+            <AboutHero></AboutHero>
+            <Location></Location>
+        </div>
+    );
+};
+
+export default About;
