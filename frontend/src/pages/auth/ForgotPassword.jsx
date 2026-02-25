668226697133250b3ed9a404f7f4ab95a9c42efa
sabbirsohag-1509 <sabbirhossainsohag1509@gmail.com>
2026-02-25 16:42:09 +0600

    forget password implement successfully

--- frontend/src/pages/auth/ForgotPassword.jsx
+++ frontend/src/pages/auth/ForgotPassword.jsx
@@ -1,28 +1,31 @@
 import React from "react";
 
 import { useForm } from "react-hook-form";
+import { useAuthStore } from "../../store/useAuthStore";
+import { Link, useNavigate } from "react-router";
 
 const ForgotPassword = () => {
+  const { resetPassword, userLoading } = useAuthStore();
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm();
 
   const onSubmit = (data) => {
-    console.log(data);
-    // backend
+    resetPassword(data.email);
+    reset();
+    navigate("/auth/login");
   };
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded-xl shadow-lg w-full max-w-md">
-        <h2 className="text-2xl font-bold mb-4 text-center">
-          Reset Password
-        </h2>
+        <h2 className="text-2xl font-bold mb-4 text-center">Reset Password</h2>
 
         <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-
           <div>
             <input
               type="email"
@@ -48,11 +51,25 @@ const ForgotPassword = () => {
 
           <button
             type="submit"
-            className="w-full bg-primary text-white py-3 rounded-lg"
+            disabled={userLoading}
+            className={`w-full py-3 rounded-lg text-white cursor-pointer ${
+              userLoading ? "bg-gray-400 cursor-not-allowed" : "bg-primary"
+            }`}
           >
-            Send Reset Link
+            {userLoading ? "Sending..." : "Send Password Reset Email"}
           </button>
         </form>
+        <div>
+          <p className="text-sm text-right mt-4">
+            Remember your password?{" "}
+            <Link
+              to="/auth/login"
+              className="text-primary font-medium hover:underline"
+            >
+              Login
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
