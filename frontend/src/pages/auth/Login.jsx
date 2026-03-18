6faacd2945aa2bd77823b3cea4804389686a5096
china akther <chinaakther05@gmail.com>
2026-03-18 21:58:40 +0600

    refactor login and register pages and improve theme design

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -8,6 +8,7 @@ import SocialLogin from "../../components/auth/SocialLogin";
 import { useAuthStore } from "../../store/useAuthStore";
 import { Link } from "react-router";
 
+
 const Login = () => {
   const {
     register,
@@ -22,15 +23,16 @@ const Login = () => {
     loginWithEP(data);
   };
   return (
-    <div className="bg-primary/30 min-h-screen flex items-center justify-center p-4">
-      <div className="bg-white rounded-2xl shadow-xl w-full max-w-5xl flex flex-col md:flex-row overflow-hidden">
-        {/* ===== Left Side (Form) ===== */}
+    <div className="bg-base-100  min-h-screen flex items-center justify-center p-4">
+      <div className="bg-base-200 rounded-2xl  shadow-xl w-full max-w-5xl flex flex-col md:flex-row overflow-hidden">
+        {/* Left Side  */}
         <div className="w-full md:w-1/2 p-6 sm:p-10 flex flex-col justify-center">
-          <div className="flex justify-center mb-4">
+          <div className="flex justify-center  mb-4">
             <Logo />
+            
           </div>
 
-          <h2 className="text-2xl sm:text-3xl font-bold text-gray-800 mb-4 text-center">
+          <h2 className="text-2xl sm:text-3xl font-bold text-base-content mb-4 text-center">
             Welcome Back! Please login
           </h2>
 
@@ -42,7 +44,7 @@ const Login = () => {
                 placeholder="Email"
                 {...register("email")}
                 required
-                className={`w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-primary`}
+                className={`w-full px-4 py-3 rounded-lg border border-gray-700 hover:border-primary focus:outline-none focus:ring-2 focus:ring-primary`}
               />
             </div>
 
@@ -64,15 +66,14 @@ const Login = () => {
                         "Password must contain at least 1 uppercase and 1 lowercase letter",
                     },
                   })}
-                  className={`w-full px-4 py-3 pr-12 rounded-lg border ${
-                    errors.password ? "border-red-500" : "border-gray-300"
-                  } focus:outline-none focus:ring-2 focus:ring-primary`}
+                  className={`w-full px-4 py-3 pr-12 rounded-lg border ${errors.password ? "border-red-500" : "border-gray-700"
+                    } focus:outline-none hover:border-primary focus:ring-2 focus:ring-primary`}
                 />
 
                 {/* Eye Button */}
                 <span
                   onClick={() => setShowPassword(!showPassword)}
-                  className="absolute right-4 top-1/2 -translate-y-1/2 cursor-pointer text-gray-500 hover:text-primary transition"
+                  className="absolute right-4 top-1/2 -translate-y-1/2 cursor-pointer text-base-content hover:text-primary transition"
                 >
                   {showPassword ? (
                     <FaEye size={20} />
@@ -98,26 +99,27 @@ const Login = () => {
             </p>
             <button
               type="submit"
-              className={`w-full bg-primary/90 hover:bg-primary cursor-pointer text-white py-3 rounded-lg font-semibold transition flex justify-center items-center gap-2 ${userLoading && "btn btn-disabled py-3 bg-primary"}`}
+              className={`relative overflow-hidden w-full bg-primary/90 hover:bg-primary cursor-pointer text-base-content hover:border-primary py-3 rounded-lg font-semibold transition flex justify-center items-center gap-2 group ${userLoading && "btn btn-disabled py-3 bg-primary"
+                }`}
               disabled={userLoading}
             >
-              {userLoading && (
-                <span className="loading loading-spinner loading-sm"></span>
-              )}
-              {userLoading ? "LoggingIn..." : "Login"}
+              <span className="relative z-10 flex items-center gap-2">
+                {userLoading && (
+                  <span className="loading loading-spinner loading-sm"></span>
+                )}
+                {userLoading ? "LoggingIn..." : "Login"}
+              </span>
+
+              <span className="absolute inset-0 bg-secondary -translate-y-full group-hover:translate-y-0 transition-transform duration-300 ease-in-out"></span>
             </button>
           </form>
 
           {/* Social Login  */}
-          {/* or  */}
-          <div className="flex items-center gap-3 my-4">
-            <div className="flex-1 h-px bg-gray-300"></div>
-            <span className="text-gray-500 text-sm font-medium">Or</span>
-            <div className="flex-1 h-px bg-gray-300"></div>
-          </div>
+         
+          <div className="divider">OR</div>
           {/* sign up with social  */}
           <SocialLogin />
-          <p className="text-gray-500 text-xs sm:text-sm mt-4 text-center">
+          <p className="text-base-content text-xs sm:text-sm mt-4 text-center">
             Are you new to our website?{" "}
             <Link
               to="/auth/register"
@@ -128,14 +130,16 @@ const Login = () => {
           </p>
         </div>
 
-        {/* ===== Right Side (Image) ===== */}
-        <div className="w-full md:w-1/2 bg-gray-50 flex items-center justify-center p-6">
+        {/*  Right Side image  */}
+        <div className="w-full md:w-1/2 bg-base-200 flex items-center justify-center p-6">
           <img
             src={imageIllus}
             alt="Chat Illustration"
             className="max-h-100 object-contain"
           />
         </div>
+
+
       </div>
     </div>
   );
